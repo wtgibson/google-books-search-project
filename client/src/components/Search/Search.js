ac2496b3ac62d6ff4cf3efcd9efe67a6cc68cbed
Gibbyswag <will.gibson7@gmail.com>
2020-05-21 14:55:47 -0700

    fixed heroku deployment issues

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -4,23 +4,7 @@ import SearchContainer from "../SearchContainer/SearchContainer.js";
 
 function Search(props) {
     return (
-        <div className="container">
-            <div className="row">
-                <h4>Book Search</h4>
-                <h6>Search for and Save Books of Interest</h6>
-            </div>
-            <div className="row">
-                <form className="col s12">
-                    <div className="row">
-                        <div className="input-field col s12">
-                        <textarea id="search" className="materialize-textarea form-control" onChange={props.handleInputChange} value={props.search} name="search" type="text"></textarea>
-                        <label for="search">Title, Author, Genre, etc.</label>
-                        <span className="helper-text" data-error="wrong" data-success="right">Book Title</span>
-                        </div>
-                    </div>
-                </form>
-                {/* <button onClick={props.handleFormSubmit} className="waves-effect waves-light btn">Search</button> */}
-            </div>
+        <div className="container">            
             <SearchContainer />
         </div>
     );
